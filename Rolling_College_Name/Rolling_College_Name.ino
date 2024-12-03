1118f8f1e20c00c73efe58e06c6d745273b02d42
Omkar Kashid <omkarkashidok@gmail.com>
2024-12-04 02:10:08 +0530

    Add files via upload

---
+++ Rolling_College_Name/Rolling_College_Name.ino
@@ -0,0 +1,132 @@
+//  Project Name : Neo Pixel 
+
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+#include <FastLED_NeoMatrix.h>
+#include <FastLED.h>
+
+#include <Wire.h>
+#include <DS1307.h>
+
+DS1307 rtc;
+/*
+
+#ifndef PSTR
+ #define PSTR 
+#endif
+*/
+#define PIN 12
+
+
+#define mw 20
+#define mh 15
+
+
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_GRBW    Pixels are wired for GRBW bitstream (RGB+W NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
+
+// Example for NeoPixel Shield.  In this application we'd like to use it
+// as a 5x8 tall matrix, with the USB port positioned at the top of the
+// Arduino.  When held that way, the first pixel is at the top right, and
+// lines are arranged in columns, progressive order.  The shield uses
+// 800 KHz (v2) pixels that expect GRB color data.
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(20, 15, PIN,
+  NEO_MATRIX_BOTTOM     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800); // changes--> LEFT, ZIGZAG,ROWS
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+void setup()
+{
+  //only set the date+time one time
+//  rtc.set(0, 17, 2, 15, 10, 2022); //08:00:00 24.12.2014 //sec, min, hour, day, month, year
+
+//  Serial.begin(9600);
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(100);
+  matrix.setTextColor(colors[0]);
+    rtc.start();
+}
+
+int x    = matrix.width();
+int pass=1;
+
+void loop() 
+{
+  rolling_message();
+//  read_time();
+
+  
+}
+//======================== Rolling Message Routine ===================================
+void rolling_message()
+{
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 3);
+//  matrix.print(F("OMKAR Kashid"));
+  matrix.print(F("WELCOME TO MODERN COLLEGE OF ENGINEERING, E&TC DEPARTMENT"));
+
+ 
+ if(--x < -356) 
+  {
+    x = matrix.width();
+    if(++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+
+  matrix.show();
+  delay(150);
+  
+}
+//========================= Clock display Routine ==================================
+
+void read_time()
+{
+  uint8_t sec, min, hour, day, month,h;
+  uint16_t year; 
+   
+  rtc.get(&sec, &min, &hour, &day, &month, &year);  //get time from RTC
+ 
+  h=hour % 12 ;
+  if(h == 0) { h=12;}
+
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 0);
+  if(h<9) { matrix.print("0"); }
+  matrix.print((h));
+  matrix.print(":");
+  matrix.setTextColor(colors[1]);
+  matrix.show();
+
+  matrix.setCursor(0, 8);
+  matrix.print((min));
+  matrix.setTextColor(colors[2]);
+  matrix.show();
+  delay(5000);
+}
+//============================= Test each LED ======================================
---
+++ test1/test1.ino
@@ -0,0 +1,208 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h> 
+
+
+#define PIN 12
+
+int delayValue=500;
+int i,j,k,m,n,x; 
+//the Wemos WS2812B RGB shield has 1 LED connected to pin 2
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(300, PIN, NEO_GRB + NEO_KHZ800);
+
+const unsigned int bitmap24[]={
+ 
+0xD1A4, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x00,    // 0x0010 1 -(00-14) pixels
+0x00, 0x00, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x7F007F, 0x0000,    // 0x0020 2 -(15-29) pixels
+
+0x0000, 0x0000, 0x7F007F, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x001f1f, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,    // 0x0030 3 -(30-44) pixels
+0x0000, 0x00, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x001f1f, 0x000, 0x0000, 0x7F007F, 0x00, 0x0000, 0x00, 0x0000,    // 0x0040 4 -(45-59) pixels
+
+0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x001f1f, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,    // 0x0050 5 -(60-74) pixels
+0x0000, 0x0000, 0x0000, 0x0000, 0x00, 0x0000, 0x0000, 0x001f1f, 0x001f1f, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,    // 0x0060 6 -(75-89) pixels
+
+0x0000, 0x0000, 0x0000, 0x7F007F, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x001f1f, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,    // 0x0070 7 -(90-104) pixels
+0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x001f1f, 0x0000, 0x0000, 0x0000, 0xD1A4, 0x00, 0x0000,    // 0x0080 8 -(105-119) pixels
+
+0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x00, 0x001f1f, 0x001f1f, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,    // 0x0090 9 -(120-134) pixels
+0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f1f, 0x001f1f, 0x00, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,    // 0x00A0 10(135-159) pixels
+
+0x0000, 0x0000, 0x0000, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0x0000, 0x0000, 0x0000,    // 0x 00B0 11-(150-164) pixels
+0x0000, 0x0000, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0x0000, 0x0000,    // 0x00C0 12-(165-179) pixels
+
+0x0000, 0xA902, 0xA902, 0xA902, 0xA902, 0x00007f, 0x00, 0x00007f, 0x00007f, 0x00007f, 0xA902, 0xA902, 0xA902, 0xA902, 0x0000,    // 0x00D0 13-(180-194) pixels
+0x0000, 0xA902, 0xA902, 0xA902, 0xA902, 0x00007f, 0x7F007F, 0x00007f, 0x7F007F, 0x00, 0xA902, 0xA902, 0xA902, 0xA902, 0x0000,    // 0x00E0 14-(195-209) pixels
+
+0x0000, 0xA902, 0xA902, 0xA902, 0xA902, 0x00007f, 0x00007f, 0x00007f, 0x00007f, 0x00007f, 0xA902, 0xA902, 0xA902, 0xA902, 0x0000,    // 0x00F0 15-(210-224) pixels
+0x0000, 0x0000, 0xA902, 0xA902, 0xA902, 0x00, 0x7F007F, 0x00007f, 0x7F007f, 0x00007f, 0xA902, 0xA902, 0xA902, 0x0000, 0x0000,    // 0x0100 16-(225-239) pixels
+
+0x0000, 0x0000, 0xA902, 0xA902, 0xA902, 0x00007f, 0x00007f, 0x00007f, 0x00, 0x00007f, 0xA902, 0xA902, 0xA902, 0x0000, 0x0000,    // 0x0110 17-(240-254) pixels
+0x0000, 0x0000, 0x0000, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0x0000, 0x0000, 0x0000,    // 0x0120 18-(255-269) pixels
+
+0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0xA902, 0x0000, 0x0000, 0x0000, 0x0000,    // 0x0130 19-(270-284) pixels
+0xD1A4, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x00    // 0x0140 20-(285-300) pixels
+
+};
+
+
+  
+void setup() 
+{
+  Serial.begin(9600);
+  pixels.begin(); // This initializes the NeoPixel library.
+ 
+}
+
+
+void loop() 
+{
+
+/*
+   for(m=0; m<300; m++)  { pixels.setPixelColor(m,0x0); }
+   pixels.show();
+
+//-----------------------------------------------------------------------------------------------------
+// Image rolling from Top to the Center of the Matrix   
+    
+//*********************************                          
+      
+      for(m=0; m<15; m++){ Serial.print(m);  Serial.print("  ");  }
+      Serial.println("\n"); 
+      delay(500);
+   
+//********************************* 
+      
+      for(m=0; m<15; m++){ Serial.print(m);  Serial.print("  ");  }
+     
+      
+      for(m=0; m<15; m++){ Serial.print(29-m);  Serial.print("  "); }
+      Serial.println("\n"); 
+      delay(500);
+//********************************* 
+      
+      for(m=0; m<15; m++){ Serial.print(m);  Serial.print("  ");  }
+      
+      for(m=0; m<15; m++){ Serial.print(29-m);  Serial.print("  "); }
+      
+      for(m=0; m<15; m++){ Serial.print(30+m);  Serial.print("  "); }
+      Serial.println("\n"); 
+      delay(500);
+//********************************* 
+    
+      for(m=0; m<15; m++){ Serial.print(m);  Serial.print("  ");  }
+     
+      for(m=0; m<15; m++){ Serial.print(29-m);  Serial.print("  "); }
+ 
+      for(m=0; m<15; m++){ Serial.print(30+m);  Serial.print("  "); }
+
+      for(m=0; m<15; m++){ Serial.print(59-m);  Serial.print("  "); }
+      Serial.println("\n"); 
+      delay(500);
+
+
+
+    delay(3000);
+
+}
+*/
+//********* THE PROGRAM END HERE **************
+/* 
+//simple function which takes values for the red, green and blue led and also
+//a delay
+void setColor(int led, int redValue, int greenValue, int blueValue, int delayValue)
+{
+  pixels.setPixelColor(led, pixels.Color(redValue, greenValue, blueValue)); 
+  pixels.show();
+  delay(delayValue);
+}
+
+*/
+
+
+
+//--------------------------------------------------------------------------------
+//Panti rolling from Right to Left code
+
+  for(i=0; i<15; i++)
+   {    
+    for(j=0; j<10; j++)
+     {
+       n= ((14-i)+(j*30)); 
+       x=j*30;
+       for(m=0; m<i+1; m++){pixels.setPixelColor(n++,bitmap24[x++]);}
+
+       x=15+(j*30);   
+       n=((15+i)+(j*30));
+       for(m=0; m<i+1; m++){pixels.setPixelColor(n--,bitmap24[x++] ); }
+     }
+      pixels.show();
+      delay(70);
+    }
+  
+ 
+ //***********************   
+
+  for(i=14; i>0; i--)
+   {    
+        
+    for(j=0; j<10; j++)
+     {
+       n=j*30;                            
+       x=((15-i)+(j*30));
+       for(m=i; m>0; m--){pixels.setPixelColor(n++,bitmap24[x++]); }
+     
+       x=((30-i)+(j*30));                                       
+       n=29+(j*30);                            
+       for(m=i; m>0; m--){pixels.setPixelColor(n--,bitmap24[x++]);  }     
+     } 
+      pixels.show();
+      delay(70);       
+    }  
+
+}
+/*
+//-----------------------------------------------------------------------------------------------------
+// Image rolling from Left to the Center of the Matrix   
+
+   for(i=0; i<14; i++)
+   {          
+    for(j=0; j<10; j++)
+     {
+       n= j*30;                       
+       x=(14-i)+(j*30);    
+       for(m=0; m<i+1; m++){pixels.setPixelColor(n++,bitmap24[x++]);}
+
+       x=(29-i)+(j*30);   
+       n=29 +(j*30);                           
+       for(m=0; m<i+1; m++){pixels.setPixelColor(n--,bitmap24[x++]);}
+     }
+     pixels.show();
+     delay(30);        
+   }  
+
+//--------------------------------------------------------------------------------------------------------
+// Image rolling from Center to the Right of the Matrix   
+
+   for(i=14; i>=0; i--)
+   {          
+    for(j=0; j<10; j++)
+     {
+       n= (14-i)+(j*30);                       
+       x=j*30;
+       for(m=0; m<i+1; m++){pixels.setPixelColor(n++,bitmap24[x++]);}
+     }
+       
+       n=(15+i) +(j*30); 
+       x=15+(j*30);                          
+       for(m=0; m<i+1; m++){pixels.setPixelColor(n--,bitmap24[x++]);}
+    }
+     pixels.show();
+     delay(30);   
+     
+     for(m=0; m<300; m++)  { pixels.setPixelColor(m,0x0); }
+     pixels.show();
+     
+    }  
+
+    delay(3000);
+*/
